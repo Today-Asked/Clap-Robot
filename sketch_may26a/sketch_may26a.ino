45690498a90e815fbf851d4eebd9b6aacd850a92
Winnie <63833544+Today-Asked@users.noreply.github.com>
2021-05-26 17:17:39 +0800

    songs
    似乎是紅蓮華

---
+++ sketch_may26a/sketch_may26a.ino
@@ -0,0 +1,1043 @@
+
+
+#include <Servo.h>
+
+Servo servoLeft;          // 宣告左邊伺服馬達
+Servo servoRight;         // 宣告右邊伺服馬達
+
+void setup() { 
+  servoLeft.attach(9);  // 將 Pin 10 指定為左邊伺服馬達
+  servoRight.attach(8);  // 將 Pin  9 指定為右邊伺服馬達
+} 
+
+void loop() {  
+  //歸零 開始囉XD 
+ 
+  //咚1
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1800);//2000-200
+//咚2
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1800);
+//咚3
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚4
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(2300);
+//咚5
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1800);
+//咚6
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1800);
+//咚7
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚8
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(3300);
+//咚9
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚10
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1800);
+//咚11
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1800);
+//咚12
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(3300);
+//咚13
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚14
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚15
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(500);
+//咚16
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚17
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚18
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(3300);
+//咚19
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚20
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1800);
+//咚21
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1800);
+//連咚22
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(2500);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚23
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚24
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚25
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚26
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚27
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚28
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1800);
+//咚29
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚30
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚31
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚32
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚33
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚34
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚35
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚36
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚37
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚38
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚39
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚40
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚41
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚42
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚43
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚44
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚45
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚46
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚47
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚48
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚49
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚50
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚51
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(3300);
+//咚52
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚53
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚54
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚55
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚56
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚57
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚58
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚59
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚60
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚61
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚62
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚63
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚64
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚65
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚66
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚67
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚68
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(3000);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(2500);
+//咚69
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+  //咚70
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+  //咚71
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+  //咚72
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚73
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚74
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚75
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚76
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚77
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(1000);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1000);
+//咚78
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1800);
+//咚79
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚80
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚81
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚82
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(3000);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(2500);
+//咚83
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚84
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚85
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚86
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚87
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚88
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚89
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚90
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚91
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚92
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚93
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚94
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚95
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚96
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚97
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚98
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚99
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚100 慶祝辣!
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(2500);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1000);
+//咚101
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚102
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(1300);
+//咚103
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚104
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(2800);
+//咚105
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(7800);
+//咚106
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚107
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚108
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(800);
+//咚109
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚110
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚111
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚112
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚113
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(300);
+//咚114
+  servoLeft.write(180);  
+  
+  servoRight.write(0);  
+  delay(200);
+  servoLeft.write(90); 
+   
+  servoRight.write(90);
+  delay(10000);
+}
